697931dd6db835efa315028f32f5cf420398ef67
Mark McCann <mjmccann12@me.com>
2020-07-29 15:33:01 -0500

    added repos

---
+++ Repositories/PersonalityTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectionPoint.Repositories
+{
+    public class PersonalityTypeRepository
+    {
+    }
+}
